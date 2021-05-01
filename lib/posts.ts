fee9164f7e6ee743fc0df340626213ae925f4d35
yoritin <47884591+yoritin@users.noreply.github.com>
2021-05-01 16:26:08 +0900

    delete comment out

--- lib/posts.ts
+++ lib/posts.ts
@@ -38,31 +38,6 @@ export function getSortedPostsData() {
   })
 }
 
-// export function getAllPostIds() {
-//   const fileNames = fs.readdirSync(postsDirectory)
-
-//   // Returns an array that looks like this:
-//   // [
-//   //   {
-//   //     params: {
-//   //       id: 'ssg-ssr'
-//   //     }
-//   //   },
-//   //   {
-//   //     params: {
-//   //       id: 'pre-rendering'
-//   //     }
-//   //   }
-//   // ]
-//   return fileNames.map((fileName) => {
-//     return {
-//       params: {
-//         id: fileName.replace(/\.md$/, ''),
-//       },
-//     }
-//   })
-// }
-
 export async function getAllPostIds() {
   const repoUrl =
     'https://api.github.com/repos/yoritin/nextjs-blog/contents/posts'
@@ -78,27 +53,6 @@ export async function getAllPostIds() {
     }
   })
 }
-
-// export async function getPostData(id) {
-//   const fullPath = path.join(postsDirectory, `${id}.md`)
-//   const fileContents = fs.readFileSync(fullPath, 'utf8')
-
-//   // Use gray-matter to parse the post metadata section
-//   const matterResult = matter(fileContents)
-
-//   // Use remark to convert markdown into HTML string
-//   const processedContent = await remark()
-//     .use(html)
-//     .process(matterResult.content)
-//   const contentHtml = processedContent.toString()
-
-//   // Combine the data with the id and contentHtml
-//   return {
-//     id,
-//     contentHtml,
-//     ...(matterResult.data as { date: string; title: string }),
-//   }
-// }
 
 export async function getPostData(id) {
   const repoUrl = `https://api.github.com/repos/yoritin/nextjs-blog/contents/posts/${id}.md`
