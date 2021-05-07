f6e3df0c5aad23b8c96b2edcfbb231f8c4b8e1bb
yoritin <47884591+yoritin@users.noreply.github.com>
2021-05-07 14:51:10 +0900

    add card

--- components/layout.tsx
+++ components/layout.tsx
@@ -27,7 +27,10 @@ export default function Layout({ children, home }: Props) {
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         /> */}
-        <meta property="og:image" content="/twitter-card.jpg" />
+        <meta
+          property="og:image"
+          content="https://twitter.com/yoriblog/header_photo"
+        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
