f8097cd6ac605e07c497960d7c8fa236d26e201f
yoritin <47884591+yoritin@users.noreply.github.com>
2022-02-16 22:55:46 +0900

    不要コメントアウト削除

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -18,12 +18,6 @@ export default function Layout({ children, home }: Props) {
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta name="description" content="Yori Portfolio" />
-        {/* <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        /> */}
         <meta
           property="og:image"
           content="https://nextjs-blog-yoritin.vercel.app/twitter-card.jpg"
