a3742f40b3301ce938cb0b8d44b0337ab39a475c
yoritin <47884591+yoritin@users.noreply.github.com>
2022-05-30 23:49:55 +0900

    fix

--- src/components/date.tsx
+++ src/components/date.tsx
@@ -1,6 +1,13 @@
 import { parseISO, format } from 'date-fns'
+import { VFC } from 'react'
 
-export default function Date({ dateString }: { dateString: string }) {
+type Props = {
+  dateString: string
+}
+
+const Date: VFC<Props> = ({ dateString }) => {
   const date = parseISO(dateString)
   return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
 }
+
+export default Date
