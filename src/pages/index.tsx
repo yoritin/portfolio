6417a95a212ab20300f6e10340f3a48722e6b6bb
yoritin <47884591+yoritin@users.noreply.github.com>
2026-01-24 01:51:36 +0900

    setup pnpm (#57)
    * setup pnpm
    
    * fix: set Node.js version to 20

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,9 +45,7 @@ const Home: NextPage<Props> = ({ allPostsData, profileData }) => {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
               <small className={utilStyles.lightText}>
                 <Date dateString={date} />
